f99bea59ca4e44e989f0595bf022fc6b1394145d
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2023-02-11 17:53:51 +0800

    Reduce sample

--- prusti-viper/src/lib.rs
+++ prusti-viper/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn build_spaghetti_domain<'v>(encoder: &Encoder<'v>, ty: Ty<'v>) -> Result<Encoder<'tcx>, ()> {
+pub fn main() -> Result<Encoder<'tcx>, ()> {
     let qweqweqweqqweq = Encoder::encode(encoder, ty)?;
 //=======================================================
 
